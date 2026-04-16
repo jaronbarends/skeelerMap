fe5a165216b2d41d04da1e99186b0ded119fa0b5
jaronbarends <731645+jaronbarends@users.noreply.github.com>
2026-04-16 20:16:39 +0200

    Remove sizes from traffic sign svgs

--- src/components/FabContainer.tsx
+++ src/components/FabContainer.tsx
@@ -1,15 +1,7 @@
 import { ReactNode } from 'react';
 
-import { getIconByName } from '@/lib/getIconByName';
-
 import styles from './FabContainer.module.css';
 
-const Icon = getIconByName('trafficSignSlope');
 export default function FabContainer({ children }: { children: ReactNode }) {
-  return (
-    <div className={styles.fabContainer}>
-      <Icon />
-      {children}
-    </div>
-  );
+  return <div className={styles.fabContainer}>{children}</div>;
 }
