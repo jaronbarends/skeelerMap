2cb1ebfc8bd3f0102a51b77c60e6533be5c7f533
jaronbarends <731645+jaronbarends@users.noreply.github.com>
2026-04-17 14:07:05 +0200

    export getServerClient function for improved accessibility in supabaseAuth

--- src/lib/supabaseAuth.server.ts
+++ src/lib/supabaseAuth.server.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-async function getServerClient() {
+export async function getServerClient() {
   const cookieStore = await cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
