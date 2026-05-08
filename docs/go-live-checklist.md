ce084ef17fd82609231192d7d0451a5a13ba6eb1
jaronbarends <731645+jaronbarends@users.noreply.github.com>
2026-05-08 15:20:45 +0200

    add middleware to handle supabase auth

---
+++ docs/go-live-checklist.md
@@ -0,0 +1,3 @@
+# Go Live checklist
+
+- Verify that email expiry is 86400 seconds (3600 \* 24) Authentication > Sign In / Providers > Email > Email OTP expiration
---
+++ src/middleware.ts
@@ -0,0 +1,38 @@
+import { createServerClient } from '@supabase/ssr';
+import { type NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  let response = NextResponse.next({ request });
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) => {
+            request.cookies.set(name, value);
+          });
+          response = NextResponse.next({ request });
+          cookiesToSet.forEach(({ name, value, options }) => {
+            response.cookies.set(name, value, options);
+          });
+        },
+      },
+    }
+  );
+  // call getUser to be able to refresh JWT
+  try {
+    await supabase.auth.getUser();
+  } catch {
+    // no real need to handle here - user will be informed when they try doing something that requires auth
+  }
+  return response;
+}
+
+// exclude next.js internals, static files, images
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'],
+};
